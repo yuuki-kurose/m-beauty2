2f997eabbb03e95545efc9ca5e6f2757c5073a22
yuuki kurose <pw1006@icloud.com>
2022-04-19 00:52:16 +0900

    vuetify日本語対応に変更

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import ja from 'vuetify/es5/locale/ja';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-});
+const opts = {
+    lang: {
+        locales: {ja},
+        current: 'ja',
+    },
+};
+export default new Vuetify(opts
+);
