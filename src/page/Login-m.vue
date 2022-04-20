2bb4d00c2c9549b1206fdc1196d26f9f81ef5a7a
yuuki kurose <pw1006@icloud.com>
2022-04-20 13:14:28 +0900

    firebase導入

--- src/page/Login-m.vue
+++ src/page/Login-m.vue
@@ -6,10 +6,10 @@
       </v-card-title>
         <v-card-text>
           <v-form>
-            <v-text-field label="ユーザー名"></v-text-field>
-            <v-text-field type="password" label="パスワード"></v-text-field>
+            <v-text-field type="email" v-model="formData.email" label="メールアドレス"></v-text-field>
+            <v-text-field type="password" v-model="formData.password" label="パスワード"></v-text-field>
             <v-card-actions> 
-              <v-btn class="info">ログイン</v-btn>
+              <v-btn class="info" v-on:click="login">ログイン</v-btn>
             </v-card-actions>
           </v-form>
         </v-card-text>  
@@ -19,6 +19,8 @@
 
 
 <script>
+  import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+  import app from '../firebase';
   export default {
     data: () => ({
       links: [
@@ -28,6 +30,26 @@
         'Registration',
         
       ],
+      formData: {
+        email: '',
+        password: ''
+      }
     }),
+    methods: {
+      login(){
+        const auth = getAuth(app);
+        signInWithEmailAndPassword(auth, this.formData.email, this.formData.password)
+        .then((userCredential) => {
+        // const user = userCredential.user;
+        console.log(userCredential);
+      })
+      // .catch((error) => {
+      //   const errorCode = error.code;
+      //   const errorMessage = error.message;
+      // });
+        console.log(this.formData)
+      }
+    }
   }
+
 </script>
