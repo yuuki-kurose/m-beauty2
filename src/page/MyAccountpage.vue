aede7ae287225012e28747cf979973ce532d37c9
yuuki kurose <pw1006@icloud.com>
2022-05-10 21:01:21 +0900

    mypage作成 #9
    途中経過（今後変更あり）

---
+++ src/page/MyAccountpage.vue
@@ -0,0 +1,28 @@
+<template>
+	<v-main>
+		<v-container>
+			<v-card>
+				<v-img height="250">
+					<v-icon size="200">mdi-account-circle</v-icon>
+				</v-img>
+
+				<v-divider></v-divider>
+
+				<v-card-title>My Page</v-card-title>
+				<v-card-subtitle>ユーザーの情報がここに反映されます</v-card-subtitle>
+			</v-card>				
+		</v-container>
+	</v-main>				
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
