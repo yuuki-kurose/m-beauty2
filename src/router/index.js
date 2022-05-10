e8433bf9fce64e7e41b42f86a6f1f96a1116095b
yuuki kurose <pw1006@icloud.com>
2022-05-11 01:13:06 +0900

    Mypage作成 #9
    ルート設定

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Login from "/src/page/Login-m.vue"
 import PostCreate from "/src/page/PostCreate-m.vue"
 import Registration from "/src/page/Registration-m.vue"
+import MyAccountpage from "/src/page/MyAccountpage.vue"
 
 
 Vue.use(VueRouter)
@@ -23,7 +24,12 @@ const routes = [
     path: '/Registration-m.vue',
     name: 'Registration',
     component: Registration
-  }
+  },
+  {
+    path: '/MyAccountpage.vue',
+    name: 'MyAccountpage',
+    component: MyAccountpage
+  },
 ]
 
 const router = new VueRouter({
